b843d446ca1841f413d1c3f6f555ef5db9c8e698
sagar-sagi <31595868+sagar-sagi@users.noreply.github.com>
2022-10-11 16:18:08 +0530

    Added the required variables and methods to calculate the statistics

--- Statistics/Statistics.cs
+++ Statistics/Statistics.cs
@@ -1,12 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Statistics
 {
     public class StatsComputer
     {
-        public Stats CalculateStatistics(List<float> numbers) {
-            //Implement statistics here
+        double average, max, min;
+        public Stats CalculateStatistics(List<double> numbers) {
+            if(numbers.Count == 0)
+            {
+                average = NaN;
+                max = NaN;
+                min = NaN;
+                return;
+            }
+            
+            //average of numbers
+            average = numbers.Average();
+            
+            //min of numbers
+            min = numbers.Min();
+            
+            //max of numbers
+            max = numbers.Max();
         }
     }
 }
